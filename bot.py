c31d08f706ec21f715125cbf8bbb614790d0387a
maicon.ferreira <maicon.ferreira@vmis.com.br>
2025-11-03 08:03:48 -0300

    ADD: Update bot

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import decimal
 from decimal import Decimal
 from datetime import datetime, timedelta
 import asyncio
+from flask import Flask
 import pytz # Importa biblioteca de fuso horário
 
 # Imports do Bot
